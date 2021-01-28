ace3e9fccc9588603b707ed8b6607b8f5f8320a5
vatgeharsha <51117677+vatgeharsha@users.noreply.github.com>
2021-01-28 20:35:01 +0530

    reverting to old proper code

--- statistics.test.py
+++ statistics.test.py
@@ -18,21 +18,21 @@ class StatsTest(unittest.TestCase):
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon) 
 
   def test_avg_is_nan_for_empty_input(self):
-    #computedStats = statistics.calculateStats([])
+    computedStats = statistics.calculateStats([])
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
     # Use nan and isnan in https://docs.python.org/3/library/math.html
-    self.assertTrue(True)
+    
     
   def test_raise_alerts_when_max_above_threshold(self):
-#    emailAlert = EmailAlert()
-#    ledAlert = LEDAlert()
-#    maxThreshold = 10.5
-#    statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
-#    statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
-#    self.assertTrue(emailAlert.emailSent)
-#    self.assertTrue(ledAlert.ledGlows)
+    emailAlert = EmailAlert()
+    ledAlert = LEDAlert()
+    maxThreshold = 10.5
+    statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
+    statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
+    self.assertTrue(emailAlert.emailSent)
+    self.assertTrue(ledAlert.ledGlows)
     self.assertTrue(True)
   
 
